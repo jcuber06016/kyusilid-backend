015e221c874c0d971f1a85f8542160657cf1c9b1
jcuber06016 <jcuber0616@gmail.com>
2023-01-18 23:14:03 +0800

    with updated API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('add-announcement',[AnnouncementController::class, 'store']);
+Route::get('get-announcement',[AnnouncementController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
